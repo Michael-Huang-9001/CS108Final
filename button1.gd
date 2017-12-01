ce9a2a76bfd9b162baaaf4660ba542d12fb050cc
fuongbregas <fuongbregas@icloud.com>
2017-11-30 16:22:08 -0800

    Auto create buttons

--- button1.gd
+++ button1.gd
@@ -2,7 +2,7 @@ extends Node
 
 onready var button1 = get_parent().get_node("Choice1")
 onready var button2 = get_parent().get_node("Choice2")
-onready var textbutton = get_parent().get_node("ScrollContainer/VBoxContainer/Button")
+
 onready var logo = get_parent().get_node("ScrollContainer/VBoxContainer/ItsAMatch")
 
 onready var c1 = File.new()
@@ -15,13 +15,13 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	#textbutton.set_disabled(true)
-	textbutton.hide()
+	
 	grey = StyleBoxFlat.new()
 	blue = StyleBoxFlat.new()
 	grey.set_bg_color(Color("#D3D3D3"))
 	blue.set_bg_color(Color("#00BFFF"))
-	button1.set('custom_styles/normal', grey)
-	button2.set('custom_styles/normal', blue)
+	#button1.set('custom_styles/normal', grey)
+	#button2.set('custom_styles/normal', blue)
 	button1.connect("pressed", self, "on_pressed1")
 	button2.connect("pressed", self, "on_pressed2")
 	button1.set_text("Hi, how's your day?")
@@ -30,15 +30,18 @@ func _ready():
 
 func on_pressed1():
 	logo.hide()
-	textbutton.set_text("Hi, how's your day?")
-	textbutton.show()
-	if(counter == 0):
-		counter = counter + 1
+	
+	
+	var nextButton = Button.new()
+	nextButton.set_owner(get_tree().get_edited_scene_root())
+	
+	nextButton.set_text("New Button")
+	get_parent().get_node("ScrollContainer/VBoxContainer").add_child(nextButton)
 
 func on_pressed2():
 	logo.hide()
 	print("false")
-	if(counter == 0):
-		button1.set_text("Do you like sexual intercourse?")
-		button2.set_text("\n\nI'm sick")
-		counter = counter + 1
+
+	button1.set_text("Do you like sexual intercourse?")
+	button2.set_text("\n\nI'm sick")
+	counter = counter + 1
