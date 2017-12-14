491cacf4ff088117a34858e8941e416c0cc1fba0
fuongbregas <fuongbregas@icloud.com>
2017-12-14 11:15:57 -0800

    Add video scences

---
+++ laughVid.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var laughVid = preload("res://Vids/laughing.ogv")
+var isPlay = 0
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	pass
+func _process(delta):
+	
